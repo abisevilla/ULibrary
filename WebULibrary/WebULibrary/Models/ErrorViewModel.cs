239c53ef2d2922616c236b5718f337c30a9d43e9
Abigail Perez <abi.sevilla09@gmail.com>
2021-08-27 14:43:07 -0600

    initial commit

---
+++ WebULibrary/WebULibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebULibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
